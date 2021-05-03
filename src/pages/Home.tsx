5810315255af08375398b7334361e0a79e6c1812
froilanenriquez3 <fenriquezthree@gmail.com>
2021-05-03 10:00:21 +0200

    Create new component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton,
+  IonList, IonItem
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import ApiController from '../components/ApiController';
 import './Home.css';
+import axios from 'axios';
 
 const Home: React.FC = () => {
+
+  const apiKey = ''
+  const endpoint = ``;
+  const url = '';
+
+  const sendGetRequest = () => {
+
+    return axios({
+      url: url,
+      method: 'get'
+    }).then(reponse => {
+
+    })
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,10 +36,13 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        {/* <ExploreContainer /> */}
+        <ApiController />
       </IonContent>
     </IonPage>
   );
 };
+
+
 
 export default Home;
