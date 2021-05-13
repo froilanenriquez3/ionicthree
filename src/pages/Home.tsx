89d0cfc4eb5c0eaebe36dac785f64c7d5a744150
froilanenriquez3 <fenriquezthree@gmail.com>
2021-05-13 09:40:08 +0200

    Add searchbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,14 +12,14 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Froilex</IonTitle>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Froilex</IonTitle>
           </IonToolbar>
         </IonHeader>
         {/* <ApiController /> */}
